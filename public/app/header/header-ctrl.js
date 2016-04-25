c3e845461e614a47250233ed96cbb6bcc326d652
nplutt <nplutt@gmail.com>
2016-04-24 22:23:16 -0500

    More header html...

--- public/app/header/header-ctrl.js
+++ public/app/header/header-ctrl.js
@@ -2,6 +2,7 @@ module.exports = function($scope) {
 
     $scope.newsFeedHtml = './app/news-feed/news-feed.html';
     $scope.searchBar = './app/search-bar/search-bar.html';
+    $scope.logo = './app/logo/logo.html';
 
     $scope.nws = [{
         name: 'Nick Plutt',
